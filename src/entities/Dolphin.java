064b44610a887316007a97447bd98df56abf03b9
ranimbensalem <160742948+ranimbensalem@users.noreply.github.com>
2025-10-23 19:16:47 +0100

    Update Dolphin.java

--- src/entities/Dolphin.java
+++ src/entities/Dolphin.java
@@ -8,21 +8,13 @@ public class Dolphin extends Aquatic {
         this.swimmingSpeed = swimmingSpeed;
     }
 
-    public float getSwimmingSpeed() {
-        return swimmingSpeed;
-    }
-
-    public void setSwimmingSpeed(float swimmingSpeed) {
-        this.swimmingSpeed = swimmingSpeed;
-    }
-
     @Override
     public void swim() {
-        System.out.println("This dolphin is swimming.");
+        System.out.println("This dolphin is swimming .");
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", swimmingSpeed=" + swimmingSpeed;
+        return super.toString() + ", swimmingSpeed= " + swimmingSpeed;
     }
 }
