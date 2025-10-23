d600cb450e299cd69823edbf8977665de63388cc
ranimbensalem <160742948+ranimbensalem@users.noreply.github.com>
2025-10-23 19:17:07 +0100

    Refactor Penguin methods for clarity and functionality

--- src/entities/Penguin.java
+++ src/entities/Penguin.java
@@ -8,16 +8,15 @@ public class Penguin extends Aquatic {
         this.swimmingDepth = swimmingDepth;
     }
 
-    public float getSwimmingDepth() {
-        return swimmingDepth;
-    }
+    public float getSwimmingDepth() { return swimmingDepth; }
 
-    public void setSwimmingDepth(float swimmingDepth) {
-        this.swimmingDepth = swimmingDepth;
+    @Override
+    public void swim() {
+        System.out.println("This penguin is swimming .");
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", swimmingDepth=" + swimmingDepth;
+        return super.toString() + ", swimmingDepth= " + swimmingDepth;
     }
 }
