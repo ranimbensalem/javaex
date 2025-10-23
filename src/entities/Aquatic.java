372ca4863a20bf90e97cc20a05a09ac4eaecd37d
ranimbensalem <160742948+ranimbensalem@users.noreply.github.com>
2025-10-23 19:16:33 +0100

    Update Aquatic.java

--- src/entities/Aquatic.java
+++ src/entities/Aquatic.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Aquatic extends Animal {
+public abstract class Aquatic extends Animal {
     protected String habitat;
 
     public Aquatic(String family, String name, int age, boolean isMammal, String habitat) {
@@ -8,20 +8,24 @@ public class Aquatic extends Animal {
         this.habitat = habitat;
     }
 
-    public String getHabitat() {
-        return habitat;
-    }
+    public String getHabitat() { return habitat; }
+    public void setHabitat(String habitat) { this.habitat = habitat; }
 
-    public void setHabitat(String habitat) {
-        this.habitat = habitat;
-    }
+    public abstract void swim();
 
-    public void swim() {
-        System.out.println("This aquatic animal is swimming.");
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Aquatic other = (Aquatic) obj;
+        return this.getName().equals(other.getName()) &&
+                this.getAge() == other.getAge() &&
+                this.habitat.equals(other.habitat);
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", habitat='" + habitat + '\'';
+        return super.toString() + ", habitatt ='" + habitat + '\'';
     }
 }
